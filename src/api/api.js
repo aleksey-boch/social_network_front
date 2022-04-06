8912e8e85175e841855c8e4a6bf226cefd0782b2
olexiyb <olexiyb@mindojo.com>
2022-04-06 20:56:07 +0300

    64

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {
+        'API-KEY': '3dc40e5a-2498-4648-8754-bcdd62cbe9be',
+    },
+});
+
+export const userAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`,)
+            .then(response => {
+                return response.data;
+            });
+    }
+
+};
