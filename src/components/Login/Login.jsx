c4b21fc528e216e5b0664c6071cc007e2ae44341
olexiyb <olexiyb@mindojo.com>
2022-04-22 09:51:33 +0300

    75_1

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import {reduxForm} from "redux-form";
+
+const LoginForm = (props) => {
+    return (
+        <form>
+            <div>
+                <input placeholder={'Login'}/>
+            </div>
+            <div>
+                <input placeholder={'Password'}/>
+            </div>
+            <div>
+                <input type={"checkbox"}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({
+    form: 'login',
+})(LoginForm)
 
 const Login = (props) => {
-    return <h1>LOGIN</h1>
+    return <div>
+        <h1>LOGIN</h1>
+        <LoginReduxForm/>
+    </div>
 }
 
 export default Login;
